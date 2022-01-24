60bcc1a9484675c68b04208f6619372037a86f31
luisa-k <98270701+luisa-k@users.noreply.github.com>
2022-01-24 15:02:51 +0100

    improvements in the authentication process

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
-import './App.css';
-import React, { Component } from 'react';
-import SignIn from './components/SignIn/SignIn';
-import Logo from './components/Logo/Logo';
-import Register from './components/Register/Register';
-import Tasks from './components/Tasks/Tasks';
-import ChangeTask from './components/ChangeTask/ChangeTask';
-import AddTask from './components/AddTask/AddTask';
+import "./App.css";
+import React, { Component } from "react";
+import SignIn from "./components/SignIn/SignIn";
+import Logo from "./components/Logo/Logo";
+import Register from "./components/Register/Register";
+import Tasks from "./components/Tasks/Tasks";
+import ChangeTask from "./components/ChangeTask/ChangeTask";
+import AddTask from "./components/AddTask/AddTask";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
+  BrowserRouter,
 } from "react-router-dom";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      path: 'signin',
-      id: '',
-      name: '',
-      email: '',
-      joined: '',
+      path: "signin",
+      id: "",
+      name: "",
+      email: "",
+      joined: "",
       tasks: [],
-      taskID: '',
-      description: '',
-      createdby: '',
-      duedate: '',
+      taskID: "",
+      description: "",
+      createdby: "",
+      duedate: "",
       loggedIn: false,
-      user: ''
-      }
+      user: "",
+    };
   }
 
   loadUser = (data) => {
@@ -38,97 +39,122 @@ class App extends Component {
         id: data.id,
         name: data.name,
         email: data.email,
-        joined: data.joined
-      }
-    })
-  }
+        joined: data.joined,
+      },
+    });
+  };
 
   loadTasks = (data) => {
     data.sort((a, b) => {
-      const aDate = new Date(a.date + ' ' + a.time)
-      const bDate = new Date(b.date + ' ' + b.time)
-      
-      return bDate.getTime() - aDate.getTime()
+      const aDate = new Date(a.date + " " + a.time);
+      const bDate = new Date(b.date + " " + b.time);
+
+      return bDate.getTime() - aDate.getTime();
     });
     this.setState({
-      tasks: data
-    })
-  }
+      tasks: data,
+    });
+  };
 
   loadOneTask = (data) => {
     this.setState({
       taskID: data.id,
       createdby: data.createdby,
       description: data.description,
-      duedate: data.duedate
-    })
-  }
+      duedate: data.duedate,
+    });
+  };
 
   updateloggedIn = (update) => {
-    this.setState ({ loggedIn: update })
-  }
+    this.setState({ loggedIn: update });
+  };
 
   isAuth = async () => {
     try {
       const response = await fetch("http://localhost:5000/verify", {
         method: "GET",
-        headers: {token: localStorage.token}
+        credentials: "include",
       });
 
       const parseRes = await response.json();
       console.log(parseRes);
-
       if (parseRes.answer === true) {
-        this.loadUser(parseRes.user)
-        fetch('http://localhost:5000/tasks', {
-          method: 'post',
-          headers: { 'Content-Type': 'application/json' },
+        this.loadUser(parseRes.user);
+        fetch("http://localhost:5000/tasks", {
+          method: "post",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
           body: JSON.stringify({
             createdby: parseRes.user.id,
-          })
+          }),
         })
-          .then(response => response.json())
-          .then(tasks => {
+          .then((response) => response.json())
+          .then((tasks) => {
             this.loadTasks(tasks);
             this.updateloggedIn(true);
-          })
+          });
       } else {
-        console.log('No login possible')
+        console.log("No login possible");
+        this.setState({
+          loggedIn: false,
+        });
       }
-    } catch(err) {
+    } catch (err) {
       console.log(err.message);
     }
-  }
+  };
 
   componentDidMount = () => {
-    this.isAuth()
-  }
+    this.isAuth();
+  };
 
   render() {
+    console.log(this.state.loggedIn);
     return (
       <Router>
         <div className="App">
           <Logo />
-          <Switch>
-            <Route path="/addtask">
-              <AddTask loadTasks={this.loadTasks} user={this.state.user} />
-            </Route>
-            <Route path="/signin">
-              <SignIn loadUser={this.loadUser} loadTasks={this.loadTasks} updateloggedIn={this.updateloggedIn} />
-            </Route>
-            <Route exact path="/">
-              {this.state.loggedIn ? <Redirect to="/tasks" /> : <SignIn loadUser={this.loadUser} loadTasks={this.loadTasks} updateloggedIn={this.updateloggedIn} />}
-            </Route>
-            <Route exact path="/register">
-              {this.state.loggedIn ? <Redirect to="/tasks" /> :  <Register loadUser={this.loadUser} loadTasks={this.loadTasks} updateloggedIn={this.updateloggedIn} />}
-            </Route>
-            <Route path="/tasks">
-              {!this.state.loggedIn ? <Redirect to="/" /> : <Tasks loadTasks={this.loadTasks} loadOneTask={this.loadOneTask} name={this.state.user.name} tasks={this.state.tasks} updateloggedIn={this.updateloggedIn} />}
-            </Route>
-            <Route path="/changetask">
-              <ChangeTask loadUser={this.loadUser} loadTasks={this.loadTasks} id={this.state.taskID} createdby={this.state.createdby} duedate={this.state.duedate} description={this.state.description} />
-            </Route>
-          </Switch>
+          {!this.state.loggedIn ? (
+            <SignIn
+              loadUser={this.loadUser}
+              loadTasks={this.loadTasks}
+              updateloggedIn={this.updateloggedIn}
+            />
+          ) : (
+            <BrowserRouter>
+              <Switch>
+                <Route path="/addtask">
+                  <AddTask loadTasks={this.loadTasks} user={this.state.user} />
+                </Route>
+                <Route exact path="/register">
+                  <Register
+                    loadUser={this.loadUser}
+                    loadTasks={this.loadTasks}
+                    updateloggedIn={this.updateloggedIn}
+                  />
+                </Route>
+                <Route path="/tasks">
+                  <Tasks
+                    loadTasks={this.loadTasks}
+                    loadOneTask={this.loadOneTask}
+                    name={this.state.user.name}
+                    tasks={this.state.tasks}
+                    updateloggedIn={this.updateloggedIn}
+                  />
+                </Route>
+                <Route path="/changetask">
+                  <ChangeTask
+                    loadUser={this.loadUser}
+                    loadTasks={this.loadTasks}
+                    id={this.state.taskID}
+                    createdby={this.state.createdby}
+                    duedate={this.state.duedate}
+                    description={this.state.description}
+                  />
+                </Route>
+              </Switch>
+            </BrowserRouter>
+          )}
         </div>
       </Router>
     );
--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,81 +1,104 @@
-import React from 'react';
-import './Tasks.css';
-import {
-    Link
-} from "react-router-dom";
+import React from "react";
+import "./Tasks.css";
+import { Link } from "react-router-dom";
 
 class Tasks extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-        }
-    }
+  constructor(props) {
+    super();
+    this.state = {};
+  }
 
-    getDate = (date) => {
-        let parseDate = new Date(date);
-        var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
-        return parseDate.toLocaleString('en-US', options)
-    }
+  getDate = (date) => {
+    let parseDate = new Date(date);
+    var options = {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
+    return parseDate.toLocaleString("en-US", options);
+  };
 
-    onDeleteTask = (id, createdby) => {
-        fetch('http://localhost:5000/deletetask', {
-          method: 'delete',
-          headers: {'Content-Type': 'application/json'},
+  onDeleteTask = (id, createdby) => {
+    fetch("http://localhost:5000/deletetask", {
+      method: "delete",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({
+        id: id,
+      }),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        fetch("http://localhost:5000/tasks", {
+          method: "post",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
           body: JSON.stringify({
-            id: id
-          })
+            createdby: createdby,
+          }),
         })
-          .then(response => response.json())
-          .then(user => {
-              fetch('http://localhost:5000/tasks', {
-                  method: 'post',
-                  headers: { 'Content-Type': 'application/json' },
-                  body: JSON.stringify({
-                      createdby: createdby
-                  })
-              })
-              .then(response => response.json())
-              .then(tasks => {
-                  this.props.loadTasks(tasks)
-                })
-          })
-      }
+          .then((response) => response.json())
+          .then((tasks) => {
+            this.props.loadTasks(tasks);
+          });
+      });
+  };
 
-    logout = (e) => {
-        e.preventDefault();
-        localStorage.removeItem("token");
-        this.props.updateloggedIn(false);
-    }
+  logout = (e) => {
+    e.preventDefault();
+    document.cookie =
+      "webtoken =; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    this.props.updateloggedIn(false);
+  };
 
-    render() {
-        return (
-            <div>
-                {`Hi ${this.props.name}! Here are your tasks :)`}<br></br><br></br>
-                <div>
-                {this.props.tasks.map((tasks, i) => (
-                    <li key={i}>
-                        <div className="card" width='50%'>
-                        {tasks.description}<br></br>
-                        {`Due Date: ${this.getDate(tasks.duedate)}`}<br></br>
-                        {`Time: ${tasks.time}`}<br></br>
-                        <Link to="/changetask">
-                            <button onClick={() => {this.props.loadOneTask(tasks)}}>Change</button>
-                        </Link><br></br>
-                        <button onClick={() => {this.onDeleteTask(tasks.id, tasks.createdby)}}>Delete</button>
-                        </div>
-                        <br></br>
-                    </li>
-                ))}
-                </div>
-                <Link to="/addtask">
-                    <button>Add Task</button>
+  render() {
+    return (
+      <div>
+        {`Hi ${this.props.name}! Here are your tasks :)`}
+        <br></br>
+        <br></br>
+        <div>
+          {this.props.tasks.map((tasks, i) => (
+            <li key={i}>
+              <div className="card" width="50%">
+                {tasks.description}
+                <br></br>
+                {`Due Date: ${this.getDate(tasks.duedate)}`}
+                <br></br>
+                {`Time: ${tasks.time}`}
+                <br></br>
+                <Link to="/changetask">
+                  <button
+                    onClick={() => {
+                      this.props.loadOneTask(tasks);
+                    }}
+                  >
+                    Change
+                  </button>
                 </Link>
-                <div>
-                    <button onClick={e => this.logout(e)}>Logout</button>
-                </div>
-            </div>
-        )
-    }
+                <br></br>
+                <button
+                  onClick={() => {
+                    this.onDeleteTask(tasks.id, tasks.createdby);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+              <br></br>
+            </li>
+          ))}
+        </div>
+        <Link to="/addtask">
+          <button>Add Task</button>
+        </Link>
+        <div>
+          <button onClick={(e) => this.logout(e)}>Logout</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Tasks;
